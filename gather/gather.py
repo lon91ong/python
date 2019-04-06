45b76d19e39365d58451cac59c220a7783c3dcc5
lon91ong <lon91ong@gmail.com>
2019-04-06 12:45:23 +0800

    Update gather.py

--- gather/gather.py
+++ gather/gather.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-删除下面的多行注释可以添加SQLite功能，
-发现WPS居然不支持SQLite，所以就删了
+
 """
 
 import sys, re, requests
 from lxml import etree 
-#import sqlite3
+import sqlite3, os
 import docx, xlwt
 from urllib.parse import quote
 import tkinter
@@ -19,13 +18,13 @@ def GetDesktopPath():
 
 def main(argv):
     # 准备消息框
-    window = tkinter.Tk()   
+    window = tkinter.Tk()
     window.wm_withdraw()
     
     try:
-        word = docx.Document(str(argv[-1]))
+        #word = docx.Document(str(argv[-1]))
         #print('接收到的文件：'+str(argv[-1]))
-        #word = docx.Document('E:\\花名\\M2019年春季学期2017级第二轮.docx')
+        word = docx.Document(r'E:\花名\M2019年春季学期2017级第二轮.docx')
         grade = re.search(r'(?<=20)\d{2}(?=级)',word.paragraphs[0].text).group()
         table = word.tables[0]
         classes = []
@@ -61,58 +60,62 @@ def main(argv):
         classes[i] = min(temp,key=len)+grade+'-'+classes[i][-1] # 存在多个匹配对象时，取最短的
     #print(classes)
     
-    '''
-    #conn = sqlite3.connect(os.path.dirname(str(argv[-1]))+'\\hwmy.db') #参数路径
-    conn = sqlite3.connect(':memory:') #内存临时
-    cursor = conn.cursor()
+    
+    conn = sqlite3.connect(os.path.dirname(os.path.realpath(__file__))+'/hwmy'+grade+'.db') #参数路径
+    #conn = sqlite3.connect(':memory:') #内存临时
+    curs = conn.cursor()
     conn.text_factory = str #lambda x: str(x, "gbk", "ignore")
     try:
-        cursor.execute('create table students'+grade+' (id varchar(12) primary key, name varchar(12) not NULL collate nocase, class text collate nocase, unique (id))')
-        cursor.execute('create table classes'+grade+' (id varchar(2), class text not NULL collate nocase, unique (class))')
+        curs.execute('select * from sqlite_master where type = "table" and name = "students"')
+        if curs.fetchone() == None:
+            curs.execute('create table students (id varchar(12) primary key, name varchar(12) not NULL collate nocase, class text collate nocase, score unsigned tinyint, unique (id))')
+        curs.execute('select * from sqlite_master where type = "table" and name = "classes"')
+        if curs.fetchone() == None:
+            curs.execute('create table classes (id varchar(2), class text not NULL collate nocase, unique (class))')
     except sqlite3.OperationalError as e:
-        print(e.args[0])
+        print("Error info:",e.args[0])
         pass
-    #cursor.executemany('INSERT INTO classes VALUES (?,?,?)',[(3,'name3',19),(4,'name4',26)])
-    '''
+    #curs.executemany('INSERT INTO classes VALUES (?,?,?)',[(3,'name3',19),(4,'name4',26)])
+    
     #花名电子表
     workbook=xlwt.Workbook(encoding="utf-8")
     worksheet = workbook.add_sheet("花名", cell_overwrite_ok=True)
     worksheet.write(0, 0,'班级')
     for i in range(len(classes)):
         worksheet.write(i+1,0,classes[i])
-        '''
+        
         try:
-            cursor.execute('insert into classes'+grade+' (id, class) values ("%s","%s")'%(str(i),classes[i]))
+            curs.execute('insert into classes (id, class) values ("%s","%s")'%(str(i),classes[i]))
         except (sqlite3.IntegrityError, sqlite3.OperationalError):
             pass
-        '''
+        
         req = requests.get('http://211.81.249.110/hmc/hmc_p.asp?trbj='+quote(classes[i],encoding='gb2312'))
         content = req.content.decode('gbk', 'ignore') # 忽略非法字符，replace则用?取代非法字符；
-        ids = re.findall(r'\d{12}', content)   # 所有的学号
+        ids = re.findall(r'\d{12}(?=</td>)', content)   # 所有的学号
         root = etree.HTML(content)
         name = ''
         for j in range(len(ids)):
             name = root.xpath('//td[text()='+ids[j]+']/following-sibling::td[1]/text()')[0].lstrip('\xa0')
             worksheet.write(i+1,j+1,name)
             worksheet.write(0,j+1,'序号'+str(j+1))
-            '''
+            
             try:
-                cursor.execute('INSERT INTO students'+grade+' (id, name, class) VALUES ("%s", "%s", "%s")' % (ids[j],name,classes[i]))
+                curs.execute('INSERT INTO students (id, name, class) VALUES ("%s", "%s", "%s")' % (ids[j],name,classes[i]))
             except (sqlite3.IntegrityError, sqlite3.OperationalError):
                 pass
         
     conn.commit()
-    '''
+    
     workbook.save(GetDesktopPath()+'\\muster.xls') #桌面路径
-    '''
-    #cursor.execute('SELECT name FROM students'+grade+' WHERE id="171304011039"')
-    #cursor.execute('SELECT * FROM classes'+grade)
-    #print(cursor.fetchone()[0])
-    #print(len(cursor.fetchall()))
-    #cursor.execute("drop table classes") #删除表
-    cursor.close()
+    
+    #curs.execute('SELECT name FROM students WHERE id="171304011039"')
+    #curs.execute('SELECT * FROM classes)
+    #print(curs.fetchone()[0])
+    #print(len(curs.fetchall()))
+    #curs.execute("drop table classes") #删除表
+    curs.close()
     conn.close()
-    '''
+    
     mbox.showinfo('完成','请在桌面查看muster.xls文件！')
 
 if __name__ == "__main__":
