785fd254bcf37fea76b79624d2d05c3c031bfacf
lon91ong <lon91ong@gmail.com>
2019-04-06 12:48:38 +0800

    操作Excel表格录入成绩到数据库

---
+++ gather/upData.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 14:50:32 2019
+操作Excel表格录入成绩到数据库
+@author: xiaoniu29
+"""
+
+import sqlite3
+import xlwings as xw
+import sys, os
+from pywintypes import com_error
+import tkinter
+import tkinter.messagebox as mbox
+
+# 准备消息框
+window = tkinter.Tk()
+window.wm_withdraw()
+#mbox.showinfo('length',__name__+'\n'+str(sys.argv))
+if __name__ == "__main__" and len(sys.argv) < 2: #命令行参数指定数据库文件
+    mbox.showerror('错误','需要指定数据库文件路径!')
+    sys.exit(0)
+else:
+    datapth = os.path.abspath(sys.argv[-1]).replace('\\','/')
+#print(datapth)
+# 初始化
+wb = xw.Book(r'scoreRecord.xlsm')
+
+if __name__ == 'updata':
+    datapth = wb.sheets[0].cells(3,1).value
+    #mbox.showinfo('Info:',datapth)
+    
+conn = sqlite3.connect(datapth)
+curs = conn.cursor()
+
+def initonce():
+    try:
+        curs.execute('SELECT class FROM classes')
+    except sqlite3.OperationalError as err:
+        print("sqlite3.OperationalError: {0}".format(err))
+        mbox.showerror('错误','数据库错误:{0}\n请查验后重试!'.format(err))
+        sys.exit(0)
+    try:
+        wb.sheets[0].cells(3,1).value=datapth
+        wb.sheets[0].cells(2,11).api.Validation.Delete()
+        wb.sheets[0].cells(2,11).api.Validation.Add(3,1,3,','.join([t[0] for t in curs.fetchall()]))
+    except com_error as err:
+        print("com_error: {0}".format(err))
+    #except:
+    #    print("Unexpected error:", sys.exc_info()[0])
+        pass
+    curs.close()
+    conn.close()
+
+if __name__ == "__main__":initonce()
+
+def downData():
+    # 清楚原有数据
+    last_r = max(5,wb.sheets[0].range('A' + str(wb.sheets[0].cells.last_cell.row)).end('up').row)
+    wb.sheets[0].range('A5:J'+str(last_r)).api.ClearContents()
+    
+    cla = wb.sheets[0].cells(2,11).value
+    #print(cla)
+    if cla == None:
+        print("没有该班级信息!")
+        sys.exit(0)
+    curs.execute('SELECT id,name FROM students where class ="'+cla+'"')
+    res = curs.fetchall()
+    for i in range(len(res)):
+        wb.sheets[0].cells(i+5,1).value = res[i][0]
+        wb.sheets[0].cells(i+5,2).value = res[i][1]
+
+def upData():
+    # 成绩所在列为第5行末列
+    n = 0
+    if wb.sheets[0].cells(3,13).api.EntireColumn.Hidden:
+        last_c = 12
+    else:
+        last_c = wb.sheets[0].cells(5, wb.sheets[0].cells.last_cell.column).end('left').column
+    for i in range(5,wb.sheets[0].range('A' + str(wb.sheets[0].cells.last_cell.row)).end('up').row+1):
+        if wb.sheets[0].cells(i,last_c).value == '': #成绩为空
+            print("成绩数据为空!")
+            continue
+        try:
+            curs.execute('UPDATE students SET score = '+str(int(wb.sheets[0].cells(i,last_c).value))+' WHERE ID = "'+wb.sheets[0].cells(i,1).value+'"')
+            n = n+1
+        except:
+            print("Unexpected error:",i,sys.exc_info()[0])
+    conn.commit()
+    mbox.showinfo('完成','成功录入 '+str(n)+' 条成绩数据!')
+
+def clean():
+    global wb, curs, conn
+    curs.close()
+    conn.close()
+    wb.sheets[0].cells(2,11).api.Validation.Delete()
+    wb.sheets[0].cells(2,11).api.ClearContents()
+    wb.sheets[0].cells(3,1).api.ClearContents()
+    wb.sheets[0].range('A5:J50').api.ClearContents()
+    wb.close()
+    sys.exit(0)
