2bf9cd9ce857245e4c3df18d223dd6ff36f62bca
lon91ong <lon91ong@gmail.com>
2019-05-29 18:10:48 +0800

    同目录查找database文件

--- gather/score_server.py
+++ gather/score_server.py
@@ -8,14 +8,21 @@ Created on Wed May 29 11:24:17 2019
 from sqlite3 import connect, OperationalError
 from socket import socket, timeout
 from time import localtime
+from os import path
+from sys import exit as sysexit
+from myMod import mbox
 
 sock = socket(2, 1) #socket.AF_INET=2, socket.SOCK_STREAM=1
 sock.bind(('0.0.0.0', 8001))
 sock.listen(5)
 if localtime().tm_mon < 8:
-    dbpath = "E:/Programs/hwmy"+str(localtime().tm_year-2)[-2:]+".db"
+    dbpath = "./hwmy"+str(localtime().tm_year-2)[-2:]+".db"
 else:
-    dbpath = "E:/Programs/hwmy"+str(localtime().tm_year-1)[-2:]+".db"
+    dbpath = "./hwmy"+str(localtime().tm_year-1)[-2:]+".db"
+if not path.isfile(dbpath):
+    print('No database file in exe path, check please!')
+    mbox('错误','未能在程序目录下找到数据库文件{}！\n请查验后重试！'.format(dbpath[2:]),'error')
+    sysexit(0)
 conn = connect(dbpath)
 curs = conn.cursor()
 while True:
