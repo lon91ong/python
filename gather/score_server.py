03940cf3c42b268edfbc68fffb9d79c1b2c5ae8f
lon91ong <lon91ong@gmail.com>
2019-05-31 18:34:00 +0800

    改用WebSocket，兼容js页端调用

--- gather/score_server.py
+++ gather/score_server.py
@@ -6,16 +6,15 @@ Created on Wed May 29 11:24:17 2019
 """
 
 from sqlite3 import connect, OperationalError
-from socket import socket, timeout
 from time import localtime
 from os import path, system
 from sys import exit as sysexit
 from myMod import mbox
+from websocket_server import WebsocketServer
+from urllib.parse import unquote
 
-system("mode con cols=72 lines=50")
-sock = socket(2, 1) #socket.AF_INET=2, socket.SOCK_STREAM=1
-sock.bind(('0.0.0.0', 8001))
-sock.listen(5)
+system("mode con cols=72 lines=30")
+server = WebsocketServer(8001,'0.0.0.0')
 if localtime().tm_mon < 8:
     dbpath = "./hwmy"+str(localtime().tm_year-2)[-2:]+".db"
 else:
@@ -24,34 +23,35 @@ if not path.isfile(dbpath):
     print('No database file in exe path, check please!')
     mbox('错误','未能在程序目录下找到数据库文件{}！\n请查验后重试！'.format(dbpath[2:]),'error')
     sysexit(0)
-conn = connect(dbpath)
-curs = conn.cursor()
-while True:
-    connection,address = sock.accept()
-    try:
-        connection.settimeout(5)
-        sql = connection.recv(1024).decode('utf-8')
-        if len(sql)>0 and sql[:6]!='UPDATE':
-            try:
-                curs.execute(sql)
-                conn.commit()
-                datas = curs.fetchall()
-                connection.send(str(datas).encode('gbk'))
-            except OperationalError as e:
-                connection.send(str(e).encode('gbk'))
-        elif len(sql)>0 and sql[:6]=='UPDATE':
-            print(sql[7:11])
-            if sql[7:11] =='list': # 批量更新成绩
-                scores = eval(sql[11:])
-                #print(scores)
-                for i in range(len(scores)):
-                    curs.execute('UPDATE students SET score = '+str(scores[i][1])+' WHERE ID = "'+scores[i][0]+'"')
-            else:
-                curs.execute(sql)
+
+def message_received(client, server, sqlcmd):
+    global dbpath
+    sqlcmd = unquote(sqlcmd)
+    print('SQL command:',sqlcmd)
+    conn = connect(dbpath)
+    curs = conn.cursor()
+    if len(sqlcmd)>0 and sqlcmd[:6].upper()=='SELECT':
+        try:
+            curs.execute(sqlcmd)
             conn.commit()
-            connection.send('"OK"'.encode('gbk'))
+            datas = list(map(list,curs.fetchall()))
+        except OperationalError as e:
+            datas = str(e)
+    elif len(sqlcmd)>0 and sqlcmd[:6].upper()=='UPDATE':
+        if sqlcmd[7:11].lower() =='list': # 批量更新成绩
+            scores = eval(sqlcmd[11:])
+            #print(scores)
+            for i in range(len(scores)):
+                curs.execute('UPDATE students SET score = '+str(scores[i][1])+' WHERE ID = "'+scores[i][0]+'"')
+            print('更新{}条记录！'.format(str(len(scores))))
         else:
-            connection.send('"Sql command format error!"'.encode('gbk'))
-    except timeout:
-        print('time out')
-        connection.close()
+            curs.execute(sqlcmd)
+        conn.commit()
+        datas = '"OK"'
+    else:
+        datas = '"Sql command format error!"'
+    server.send_message(client,str(datas))
+    conn.close()
+
+server.set_fn_message_received(message_received)
+server.run_forever()
