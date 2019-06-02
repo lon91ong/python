748352b644622a9c0b9682ec76e797e47744f395
lon91ong <lon91ong@gmail.com>
2019-06-02 16:29:54 +0800

    逻辑优化

--- gather/updata_net.py
+++ gather/updata_net.py
@@ -6,48 +6,49 @@ Created on Fri Apr  5 14:50:32 2019
 """
 
 import xlwings as xw
-import sys, os
+import sys
 from pywintypes import com_error
 from urllib.parse import quote
 from myMod import mbox
 
-# 初始化,防止重复打开
+try:
+    wb = xw.apps.active.books[sys.argv[2]]
+except:
+    print("Unexpected error:",sys.exc_info()[0])
+    mbox('错误','服务性后台程序，勿点击运行！','error')
+    sys.exit(0)
+
 def netsql(sqlcmd):
     from websocket import create_connection
-    from time import sleep
+    #from time import sleep
     try:
         ws = create_connection('ws://10.0.18.207:8001')
-        sleep(1)
-        ws.send(sqlcmd)
-        sleep(1)
-        datas = eval(ws.recv()) #eval(sock.recv(1280).decode('gbk'))
     except:
         print("Server connect error! Code:",sys.exc_info()[1])
         datas = []
     else:
+        ws.send(sqlcmd)
+        #sleep(1)
+        datas = eval(ws.recv()) #eval(sock.recv(1280).decode('gbk'))
         ws.close()
     return datas
 
-def initonce(filename):
-    wb = xw.apps.active.books[filename]
+def initonce():
     classes = netsql('SELECT class FROM classes')
-    if len(classes)==0:
-        mbox('错误','数据库服务连接失败！\n请查验后重试！','error')
-        sys.exit(0)
-    if wb == None:
-        wb = xw.Book(getattr(sys,'_MEIPASS',os.path.dirname(os.path.realpath(__file__)))+r'\scoreRecord.xlsm')
-        wb.sheets[0].cells(4,1).value = os.path.realpath(sys.argv[0])
-    try:
-        wb.sheets[0].api.Unprotect()
-        wb.sheets[0].cells(2,11).api.Validation.Delete()
-        wb.sheets[0].cells(2,11).api.Validation.Add(3,1,3,','.join([t[0] for t in classes]))
-        wb.sheets[0].api.Protect()
-    except com_error as err:
-        print("com_error: {}".format(err))
-        pass
-    
-def downData(filename):
-    wb = xw.apps.active.books[filename]
+    if len(classes) == 0:
+        mbox('错误','班级列表为空！\n请查验后重试！','error')
+    else:
+        try:
+            wb.sheets[0].api.Unprotect()
+            wb.sheets[0].cells(2,11).api.Validation.Delete()
+            wb.sheets[0].cells(2,11).api.Validation.Add(3,1,3,','.join([t[0] for t in classes]))
+            wb.sheets[0].api.Protect()
+        except com_error as err:
+            print("com_error: {}".format(err))
+            pass
+    return
+
+def downData():
     # 清楚原有数据
     last_r = max(5,wb.sheets[0].range('A' + str(wb.sheets[0].cells.last_cell.row)).end('up').row)
     wb.sheets[0].range('A5:J'+str(last_r)).api.ClearContents()
@@ -56,12 +57,11 @@ def downData(filename):
     #mbox('信息','班级：'+cla,'info')
     if cla == None:
         print("没有该班级信息!")
-        sys.exit(0)
-    # 从数据库读取学生信息
-    wb.sheets[0].range("A5").value = netsql('SELECT id,name FROM students where class ="'+quote(cla)+'"')
+    else:
+        wb.sheets[0].range("A5").value = netsql('SELECT id,name FROM students where class ="'+quote(cla)+'"')
+    return
 
-def upData(filename):
-    wb = xw.apps.active.books[filename]
+def upData():
     # 成绩所在列为第5行末列
     last_c = wb.sheets[0].cells(5, wb.sheets[0].cells.last_cell.column).end('left').column # 末列，忽略隐藏列
     last_r = max(5,wb.sheets[0].range('A' + str(wb.sheets[0].cells.last_cell.row)).end('up').row) # 末行
@@ -71,23 +71,22 @@ def upData(filename):
         if wb.sheets[0].cells(i,last_c).value == '': #成绩为空
             print("成绩数据为空!")
             continue
-        scores = scores + [(wb.sheets[0].cells(i,1).value,int(round(wb.sheets[0].cells(i,last_c).value * 100) / 100.0))]
+        scores += [(wb.sheets[0].cells(i,1).value,int(round(wb.sheets[0].cells(i,last_c).value * 100) / 100.0))]
         #curs.execute('UPDATE students SET score = '+str(int(wb.sheets[0].cells(i,last_c).value))+' WHERE ID = "'+wb.sheets[0].cells(i,1).value+'"')
         n = n+1
     status = netsql('UPDATE list'+str(scores))
     if status == 'OK':
         mbox('完成','成功录入 '+str(n)+' 条成绩数据!','info')
-    #mbox.showinfo('完成','成功录入 '+str(n)+' 条成绩数据!')
+    return
 
 def main(argv):
-    if len(argv) <2: # 直接执行程序
-        mbox('错误','服务性后台程序，勿点击运行！','error')
-    elif argv[0] =='init':
-        initonce(argv[1])
+    #if len(argv) <2: # 直接执行程序
+    if argv[0] =='init':
+        initonce()
     elif argv[0] =='down':
-        downData(argv[1])
+        downData()
     elif argv[0] =='up':
-        upData(argv[1])
+        upData()
     sys.exit(0)
 
 if __name__ == "__main__":
