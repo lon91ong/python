f7f0e703fc252c6968816e1bb3caeb42aad5e275
lon91ong <lon91ong@gmail.com>
2019-11-30 13:22:33 +0800

    Update scsvr_falcon.py

--- gather/scsvr_falcon.py
+++ gather/scsvr_falcon.py
@@ -16,12 +16,23 @@ import falcon
 from json import dumps,loads
 from waitress import serve
 
+from falcon.http_status import HTTPStatus
+
+class HandleCORS(object):
+    def process_request(self, req, resp):
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header('Access-Control-Allow-Methods', '*')
+        resp.set_header('Access-Control-Allow-Headers', '*')
+        resp.set_header('Access-Control-Max-Age', 1728000)  # 20 days
+        if req.method == 'OPTIONS':
+            raise HTTPStatus(falcon.HTTP_200, body='\n')
+
 class Resource(object):
     def __init__(self, dbpth, act):
         self.dbpth = dbpth
         self.act = act
     def dict_factory(self, cursor, row):
-        return dict((col[0], row[idx]) for idx, col in enumerate(cursor.description))
+            return dict((col[0], row[idx]) for idx, col in enumerate(cursor.description))
     def on_get(self, req, resp):
         conn = connect(self.dbpth)
         #print('Query Cmd:', unquote(req.url))
@@ -30,10 +41,15 @@ class Resource(object):
             curs.execute('SELECT class FROM classes')
             resp.body = ','.join(t[0] for t in map(list,curs.fetchall()))
         elif self.act=='class':
-            conn.row_factory = self.dict_factory
-            curs = conn.cursor()
-            curs.execute('SELECT id,name FROM students where class ="'+unquote(req.query_string)+'"')
-            resp.body = dumps(curs.fetchall(),ensure_ascii=False)
+            if req.headers["CLIENT"]=='Excel':
+                conn.row_factory = self.dict_factory
+                curs = conn.cursor()
+                curs.execute('SELECT id,name,score FROM students where class ="'+unquote(req.query_string)+'"')
+                resp.body = dumps(curs.fetchall(),ensure_ascii=False)
+            else: # Greasemonkdy Script get scores to fill Web Table
+                curs = conn.cursor()
+                curs.execute('SELECT id,score FROM students where class ="'+unquote(req.query_string)+'"')
+                resp.body = dumps(dict(curs.fetchall()),ensure_ascii=False)
         else:
             resp.body = '非法途径访问！'
             #resp.body = dumps({"success":False,"Contents":"Bad Request Command!"})
@@ -63,14 +79,10 @@ if not path.isfile(dbpath):
     mbox('错误','未能在程序目录下找到数据库文件{}！\n请查验后重试！'.format(dbpath[2:]),'error')
     sysexit(0)
     
-app = falcon.API()
+app = falcon.API(middleware=[HandleCORS()])
 
 app.add_route('/classes', Resource(dbpath,'classes'))
 app.add_route('/class', Resource(dbpath,'class'))
 app.add_route('/updata', Resource(dbpath,'updata'))
 
 serve(app, listen='*:8001')
-
-
-
-
