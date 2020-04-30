a00de0c01df6871bc84460b1604511a2873da1d5
lon91ong <lon91ong@gmail.com>
2020-04-30 16:04:33 +0800

    改用list.pop(0)

--- pinyinGBK.py
+++ pinyinGBK.py
@@ -6,12 +6,12 @@ Created on Sun Apr 26 15:04:14 2020
 """
 
 def CnFirstLetter(stc):
+    stc = list(stc)
+    abc_z = 'abcdefghjklmnopqrstwxyz'
     gbkls = [b'\xb0\xc5',b'\xb2\xc1',b'\xb4\xee',b'\xb6\xea',b'\xb7\xa2',b'\xb8\xc1',b'\xb9\xfe', \
          b'\xbb\xf7',b'\xbf\xa6',b'\xc0\xac',b'\xc2\xe8',b'\xc4\xc3',b'\xc5\xb6',b'\xc5\xbe',b'\xc6\xda', \
          b'\xc8\xbb',b'\xc8\xf6',b'\xcb\xfa',b'\xcd\xda',b'\xce\xf4',b'\xd1\x89',b'\xd4\xd1',b'\xd7\xfa']
-    abc_z = 'abcdefghjklmnopqrstwxyz'
     result = ''
-    stc = list(stc)
     while len(stc) > 0:
         ssc = stc.pop(0).encode('gbk')
         if ssc>b'\xb0\xa0' and ssc<b'\xd7\xfa':
