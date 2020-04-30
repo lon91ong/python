e7c6572222d5029615c32b29a8b0607936c99098
lon91ong <lon91ong@gmail.com>
2020-04-30 15:46:44 +0800

    从map()改为list.pop(0)

--- pinyinGBK.py
+++ pinyinGBK.py
@@ -6,18 +6,17 @@ Created on Sun Apr 26 15:04:14 2020
 """
 
 def CnFirstLetter(stc):
-    if len(stc)>1: # Multi
-        return ''.join(list(map(CnFirstLetter,stc)))
-    else:   # Single
-        stc=stc.encode('gbk')
-        gbkls = [b'\xb0\xc5',b'\xb2\xc1',b'\xb4\xee',b'\xb6\xea',b'\xb7\xa2',b'\xb8\xc1',b'\xb9\xfe',b'\xbb\xf7', \
-                 b'\xbf\xa6',b'\xc0\xac',b'\xc2\xe8',b'\xc4\xc3',b'\xc5\xb6',b'\xc5\xbe',b'\xc6\xda',b'\xc8\xbb', \
-                 b'\xc8\xf6',b'\xcb\xfa',b'\xcd\xda',b'\xce\xf4',b'\xd1\x89',b'\xd4\xd1',b'\xd7\xfa']
-        abc_z = 'abcdefghjklmnopqrstwxyz'
-        
-        if stc<b'\xb0\xa1' or stc>b'\xd7\xf9':
-            return ''
-        else:
+    gbkls = [b'\xb0\xc5',b'\xb2\xc1',b'\xb4\xee',b'\xb6\xea',b'\xb7\xa2',b'\xb8\xc1',b'\xb9\xfe', \
+         b'\xbf\xa6',b'\xc0\xac',b'\xc2\xe8',b'\xc4\xc3',b'\xc5\xb6',b'\xc5\xbe',b'\xc6\xda',b'\xc8\xbb', \
+         b'\xbb\xf7',b'\xc8\xf6',b'\xcb\xfa',b'\xcd\xda',b'\xce\xf4',b'\xd1\x89',b'\xd4\xd1',b'\xd7\xfa']
+    abc_z = 'abcdefghjklmnopqrstwxyz'
+    result = ''
+    stc = list(stc)
+    while len(stc) > 0:
+        ssc = stc.pop(0).encode('gbk')
+        if ssc>b'\xb0\xa0' and ssc<b'\xd7\xfa':
             for i in range(23):
-                if stc < gbkls[i]:
-                    return abc_z[i]
+                if ssc < gbkls[i]:
+                    result += abc_z[i]
+                    break
+    return result
