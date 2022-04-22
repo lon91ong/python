a839db8b012b041d196e1f179b4503f3aa6a1856
lon91ong <lon91ong@gmail.com>
2022-04-22 20:06:26 +0800

    去黑窗, 无回显信息

--- laboffline/pySvr.py
+++ laboffline/pySvr.py
@@ -5,14 +5,24 @@
 """
 import falcon
 from waitress import serve
-from os import system
+#from os import system
 from sys import argv
 from os.path import basename
 from webbrowser import open as wOpen
 from falRes import FalRes
 
+def releasePort(port):
+    from os import popen
+    rut=popen('netstat -aon | findstr {}'.format(port)).read()
+    try:
+        pids = [u[-10:].lstrip() for u in rut.split('\n') if 'LISTENING' in u]
+        for pid in pids:
+            popen('taskkill -f -pid {}'.format(pid))
+    except:
+        pass
+
 if __name__ == '__main__':
-    system('mode con cols=42 lines=29')
+    #system('mode con cols=42 lines=29')
     svrRes = FalRes()
     app = falcon.API()
     apilst = ['/Upload/lab/{labfile}','/BizService.svc','/ServiceAPI/SetLabTimeRecordStart','/ServiceAPI/UpdateRecord','/FileTransfer.svc']
@@ -22,5 +32,5 @@ if __name__ == '__main__':
         wOpen("http://127.0.0.1:9542/Upload/lab/0")
     elif argv[1][-3:]=='xml':
         wOpen("http://127.0.0.1:9542/Upload/lab/"+basename(argv[1]))
+    releasePort('9542') #释放端口
     serve(app, listen = '127.0.0.1:9542')
-    
