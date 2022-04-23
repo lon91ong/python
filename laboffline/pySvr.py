ff4a88015e3fec6da5b5254ce2a18e7783804efb
lon91ong <lon91ong@gmail.com>
2022-04-23 18:49:54 +0800

    放弃使用releasePort

--- laboffline/pySvr.py
+++ laboffline/pySvr.py
@@ -3,23 +3,16 @@
 @Author  : lon91ong (lon91ong@gmail.com)
 @Version : 0.9.9
 """
-from os import popen
-
-def releasePort(port):
-    pps=popen('netstat -aon | findstr {}'.format(port)).read()
-    for pid in [u.split()[-1] for u in pps.split('\n') if 'LISTENING' in u]:
-        popen('taskkill -f -pid {}'.format(pid))
-
 if __name__ == '__main__':
     from sys import argv
     from time import sleep
+    from os import popen
     from os.path import basename
     from falRes import FalRes, Serv
     from webbrowser import open as wOpen
     
     svrRes = FalRes()
     port = argv[1] if len(argv)==2 and argv[1].isdigit() else '9542'
-    releasePort(port) #释放端口
     mySvr=Serv(1,svrRes,port)
     mySvr.daemon = True #服务线程与主线程共存亡
     mySvr.start()
