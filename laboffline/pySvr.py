d1704bda2a30dd5f590c1d258d10fb251c91b1bc
lon91ong <lon91ong@gmail.com>
2022-04-23 18:55:19 +0800

    完善参数传递端口

--- laboffline/pySvr.py
+++ laboffline/pySvr.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
     mySvr=Serv(1,svrRes,port)
     mySvr.daemon = True #服务线程与主线程共存亡
     mySvr.start()
-    if len(argv)==1: # 直接打开程序的情况
-        wOpen('http://127.0.0.1:{}/Upload/lab/0'.format(port))
-    elif argv[1][-3:]=='xml':
+    if argv[1][-3:]=='xml':
         wOpen('http://127.0.0.1:{}/Upload/lab/'.format(port)+basename(argv[1]))
+    else:
+        wOpen('http://127.0.0.1:{}/Upload/lab/0'.format(port))
     sleep(90)
     while(popen('tasklist | findstr "WebLabClient"').read()):
         sleep(60) #每分钟检测一次
