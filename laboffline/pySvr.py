ba8241b14d7646b3d0ae546b6b0c80ea84185cc0
lon91ong <lon91ong@gmail.com>
2022-04-23 11:47:29 +0800

    每分钟检测退出

--- laboffline/pySvr.py
+++ laboffline/pySvr.py
@@ -9,28 +9,28 @@ from waitress import serve
 from sys import argv
 from os.path import basename
 from webbrowser import open as wOpen
-from falRes import FalRes
+from falRes import FalRes, Serv
+from os import popen
 
 def releasePort(port):
-    from os import popen
     rut=popen('netstat -aon | findstr {}'.format(port)).read()
-    try:
-        pids = [u[-10:].lstrip() for u in rut.split('\n') if 'LISTENING' in u]
-        for pid in pids:
-            popen('taskkill -f -pid {}'.format(pid))
-    except:
-        pass
+    pids = [u.split()[-1] for u in rut.split('\n') if 'LISTENING' in u]
+    for pid in pids:
+        popen('taskkill -f -pid {}'.format(pid))
 
 if __name__ == '__main__':
+    from time import sleep
     #system('mode con cols=42 lines=29')
     svrRes = FalRes()
-    app = falcon.API()
-    apilst = ['/Upload/lab/{labfile}','/BizService.svc','/ServiceAPI/SetLabTimeRecordStart','/ServiceAPI/UpdateRecord','/FileTransfer.svc']
-    for apistr in apilst: app.add_route(apistr, svrRes)
-    print("\n运行中...\n")
+    port = '9542'
+    releasePort(port) #释放端口
+    mySvr=Serv(1,svrRes,port)
+    mySvr.daemon = True #服务线程与主线程共存亡
+    mySvr.start()
     if len(argv)==1: # 直接打开程序的情况
         wOpen("http://127.0.0.1:9542/Upload/lab/0")
     elif argv[1][-3:]=='xml':
         wOpen("http://127.0.0.1:9542/Upload/lab/"+basename(argv[1]))
-    releasePort('9542') #释放端口
-    serve(app, listen = '127.0.0.1:9542')
+    sleep(90)
+    while(popen('tasklist | findstr "WebLabClient"').read()):
+        sleep(60) #每分钟检测一次
