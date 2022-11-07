8a1c683b7b4885b4a6f68147c5d2b061bab2e5a0
lon91ong <lon91ong@gmail.com>
2022-11-07 11:20:11 +0800

    跳过本地程序，直接线上查询项目

--- laboffline/lsLab.py
+++ laboffline/lsLab.py
@@ -5,72 +5,37 @@
 参数说明: 三种情况 [[port], [start, end], [port, start, end]]
 """
 from os import system
-from falRes import lab_root, FalRes, Serv, downSvr
+from falRes import downSvr
+import requests
+from xml.etree.ElementTree import fromstring
+from json import loads
 
 system('mode con cols=52 lines=120')
 
-# falcon类
-class QuRes(FalRes):
-    def __init__(self):
-        #super().__init__()
-        self.curLab = ''
-        self.labSort = '未分类'
-        self.lablist = open('lablist.xml',mode='w+',encoding='utf-8')
-        self.lablist.write('<?xml version="1.0" encoding="utf-8"?>\n<root>')
-        self.port = downSvr["Port"]
-        self.fileSvr = downSvr["Host"]
-        
-    def on_get(self, req, resp, labfile):
-        from falcon import HTTP_400
-        #print('URL:{}\nPath:{}'.format(req.url,req.path))
-        print('LabID:{},LabName:{}'.format(self.curLab,labfile[:-4]))
-        self.lablist.write('  <Experiment Sort="{}" Name="{}" UpTime="2021-11-11T11:11:11" ID="{}" />\n'.format(self.labSort,labfile[:-4],self.curLab))
-        self.curLab = labfile[:-4]
-        resp.status = HTTP_400
-
-def cleanDir(dir, sname):
-    from os import path, listdir, rmdir, remove
-    for dire in listdir(dir):
-        dire = path.join(dir,dire)
-        if path.isfile(dire):
-            if sname in path.basename(dire) or path.basename(dire)[:-4] in sname:
-                remove(dire)
-                #print('删除文件:'+path.basename(dire))
-                if not listdir(path.dirname(dire)):
-                    rmdir(path.dirname(dire)) #删除空路径
-        else:
-            cleanDir(dire,sname) #递归子目录
-
 if __name__ == '__main__':
-    from subprocess import Popen
     from time import sleep
     from sys import argv
-    from base64 import b64encode
     from itertools import chain
     
-    port = argv[1] if len(argv) in [2,4] else '9650' # 指定端口
-    falRes = QuRes()
-    mySvr=Serv(falRes,port)
-    mySvr.daemon = True #服务线程与主线程共存亡
-    mySvr.start()
-    exeProg = lab_root+r'\Download\Updata\WebLabClient.exe'
+    lablist = open('lablist.xml',mode='w+',encoding='utf-8')
+    lablist.write('<?xml version="1.0" encoding="utf-8"?>\n<root>')    
+    #现有的实验分布在356~396,500~512区间;
     chal = range(int(argv[-2]),int(argv[-1])) if len(argv)>2 else chain(range(355,400),range(500,520))
-    sleep(2)
-    if(mySvr.is_alive()):
-        print("查询到如下的实验项目:")
-        #现有的实验分布在356~396,500~512区间; 本地已有的不显示
-        for labId in chal:
-            falRes.curLab = str(labId)
-            burl = bytes('/'+str(labId)+'/127.0.0.1/'+port+'/userid/op/1/2',encoding='utf-8')
-            href='lab://{}/'.format(b64encode(burl).decode('utf-8'))
-            pp = Popen([exeProg, href])
-            sleep(1)
-            pp.kill()
-            sleep(0.5)
-            if not falRes.curLab.isdigit():
-                cleanDir(lab_root+r'\Download\Updata\Download', falRes.curLab)
-        print("\n项目查询完毕!")
-    else:
-        print("\n服务端启动失败, 查询程序退出!")
-    falRes.lablist.write('</root>')
-    falRes.lablist.close()
+    headers = {'Content-Type': 'text/xml; charset=utf-8','SOAPAction': 'http://www.ustcori.com/2009/10/IBizService/DoService','Host': f'{downSvr["Host"]}:{downSvr["Port"]}'}
+    #在线查询
+    print("在线查询到如下的实验项目:")
+    for labId in chal:
+        xmls = f'<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Body><DoService xmlns="http://www.ustcori.com/2009/10"><request xmlns:i="http://www.w3.org/2001/XMLSchema-instance"><BizCode>UstcOri.BLL.BLLLabClent</BizCode><EnableCache>false</EnableCache><MethodName>FindByLABID</MethodName><Parameters xmlns:a="http://schemas.microsoft.com/2003/10/Serialization/Arrays"><a:KeyValueOfstringanyType><a:Key>LabID</a:Key><a:Value i:type="b:string" xmlns:b="http://www.w3.org/2001/XMLSchema">"{labId}"</a:Value></a:KeyValueOfstringanyType></Parameters></request></DoService></s:Body></s:Envelope>'
+        xmls = requests.post(f'http://{downSvr["Host"]}:{downSvr["Port"]}/BizService.svc',data=xmls,headers=headers).text
+        if len(xmls) > 390:
+            labDic = loads(fromstring(xmls).findall(".//{http://www.ustcori.com/2009/10}DataString")[0].text[1:-1])
+            lablist.write(f'  <Experiment Sort="{labDic["LABTYPENAME"]}" Name="{labDic["LABNAME"]}" UpTime="{labDic["UpTime"]}" ID="{labId}" LabFile="{labDic["LabFileUrl"]}" />\n')
+            print(f'LabID:{labId},LabName:{labDic["LABNAME"]}')
+        else:
+            sleep(0.7)
+            continue
+    print("\n项目查询完毕!")
+    #print("\n服务端启动失败, 查询程序退出!")
+    lablist.write('</root>')
+    lablist.close()
+    sleep(5)
