64b3a9bf7ee518797815a68d77b18e862c6aad51
lon91ong <lon91ong@gmail.com>
2022-11-20 11:12:11 +0800

    Update WXPatch.py

--- WXPatch.py
+++ WXPatch.py
@@ -7,12 +7,13 @@ import requests
 from os import path, getenv, startfile
 from sys import exc_info
 from base64 import b64decode
+from json import loads
 from re import split, search, sub
 from urllib3 import disable_warnings
 
 disable_warnings()
 app_root = path.dirname(path.realpath(__file__))
-wx_exe = (app_root if path.isfile(app_root + r'\WinXray.exe') else path.dirname(app_root)) + r'\WinXray.exe'
+wx_exe = app_root if path.isfile(app_root + r'\WinXray.exe') else path.dirname(app_root) + r'\WinXray.exe'
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
 }
@@ -25,20 +26,24 @@ def decode_b64(data):
     return urlsafe_b64decode(data).decode()
 
 def getProxy(rawurl):
-    urlst = [rawurl]+['https://raw.fastgit.org/lon91ong/python/master/sslink64',
+    urlst = [rawurl]+['https://raw.iqiq.io/lon91ong/python/master/sslink64',
+             'https://raw.fastgit.org/lon91ong/python/master/sslink64',
              'https://fastly.jsdelivr.net/gh/lon91ong/python@master/sslink64',
              'https://cdn.staticaly.com/gh/lon91ong/python/master/sslink64',
              'https://gcore.jsdelivr.net/gh/lon91ong/python@master/sslink64']
     proxies = []
     for ral in urlst:
+        print(ral)
         try:
             resp = requests.get(ral, headers=headers, verify=False, timeout=6)
         except:
             print(f'{ral} get failed with error:', exc_info()[0])
             continue
         else:
+            print(resp.content)
             proxies = [ral] + b64decode(resp.content).decode().split('\n')
             break
+    print(proxies)
     return proxies
 
 with open(path.join(app_root, 'proxy_bak.table'), 'r', encoding='utf-8-sig') as f:
@@ -46,8 +51,9 @@ with open(path.join(app_root, 'proxy_bak.table'), 'r', encoding='utf-8-sig') as 
 f.close()
 
 try:
-    proxies = getProxy(search('http[\w,:/\.\-]+', prtab[4])[0])
-    prtab[4] = sub('http[\w,:/\.\-]+', proxies[0], prtab[4], 1)
+    proxies = getProxy(search('http[\w,:/\.\-\@]+', prtab[4])[0])
+    prtab[4] = sub('http[\w,:/\.\-\@]+', proxies[0], prtab[4], 1)
+
     n = 1
     result = ''
     for locp in proxies[1:]:
@@ -57,7 +63,19 @@ try:
                       f'obfsParam="";port="{ssrl[1]}";' + f'subscribeUrl="{proxies[0]}";' + \
                       f'address="{ssrl[0]}";obfs="{ssrl[4]}";' + 'ps="{}";'.format(decode_b64(ssrl[-3])) + \
                       f'network="{ssrl[2]}";security="{ssrl[3]}"' + '};'
-            n += 1
+            n += 1; continue
+        elif locp[:5] == 'vmess':
+            ssrl = loads(decode_b64(locp[8:]))
+            result += f'[{n}' + ']={' + f'tls="{ssrl["tls"]}";path="{ssrl["path"]}";protocol="vmess";alterId={ssrl["aid"]};host="{ssrl["host"]}";' + \
+                      f'port="{ssrl["port"]}";alpn="{ssrl["alpn"]}";scy="{ssrl["scy"]}";["type"]="{ssrl["type"]}";network="{ssrl["net"]}";' + \
+                      f'subscribeUrl="{proxies[0]}";security="auto";address="{ssrl["add"]}";id="{ssrl["id"]}";sni="{ssrl["sni"]}";ps="{ssrl["ps"]}"'+ '};'
+            n += 1; continue
+        elif locp[:5] == 'ss://':
+            strs = split('@',locp[5:])
+            ssrl = split(':', decode_b64(strs[0])) + split(':|#',strs[1])
+            result += f'[{n}' + ']={' + f'port={ssrl[3]};network="tcp";address="{ssrl[2]}";id="{ssrl[1]}";' + \
+                      f'ps="{ssrl[4]}";protocol="shadowsocks";security="{ssrl[0]}"' + '};'
+            n += 1; continue
     print(f'Total of {n - 1} SSR nodes written!')
     prtab[5] = 'outbounds={' + result[:-1] + '};'
     with open(getenv('LocalAppData') + '/winXray/proxy.table', 'w', encoding='utf-8-sig') as f:
@@ -65,5 +83,6 @@ try:
     f.close()
 except:
     print("Unexpected error:", exc_info()[0:2])
+    pass
 finally:
     startfile(wx_exe)
